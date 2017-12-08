e37a9b039dcdf4682a635478853127191f669ca7
oraclerevolution <mac.n.drix@gmail.com>
2017-12-08 12:30:42 +0000

    total terminé

--- total.js
+++ total.js
@@ -7,13 +7,25 @@
  * @next multiply
  */
 
+const total = tableau => {
+  let i = 0
+  let sum = 0
 
+  while (i < tableau.length) {
+    sum += tableau[i]
+    i++
+  }
+
+  return sum
+}
+
+console.log(total([ 1, 1, 4, 5 ]))
 //* Begin of tests
 const assert = require('assert')
 
 assert.strictEqual(typeof total, 'function')
 assert.strictEqual(total.length, 1)
-assert.deepStrictEqual(total([ 1, 1, 1]), 3)
-assert.deepStrictEqual(total([ 10, 10, 10]), 30)
+assert.deepStrictEqual(total([ 1, 1, 1 ]), 3)
+assert.deepStrictEqual(total([ 10, 10, 10 ]), 30)
 assert.deepStrictEqual(total([ 24, -10, 10, 0, 0, 100 ]), 124)
 // End of tests */

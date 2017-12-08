374380d41be8f97c4a0232326b38ce64b9bf7c99
oraclerevolution <mac.n.drix@gmail.com>
2017-12-08 17:19:35 +0000

    tous mes fichiers

--- words.js
+++ words.js
@@ -6,8 +6,6 @@
  *
  * @next total
  */
-
-
 //* Begin of tests
 const assert = require('assert')
 
@@ -16,5 +14,5 @@ assert.strictEqual(words.length, 1)
 assert.deepStrictEqual(words('a b c'), [ 'a', 'b', 'c' ])
 assert.deepStrictEqual(words('a 1 c'), [ 'a', '1', 'c' ])
 assert.deepStrictEqual(words('a 1 c d e f'), [ 'a', '1', 'c', 'd', 'e', 'f' ])
-assert.deepStrictEqual(words('wol.lol lo,lol'), [ 'wol.lol', 'lo,lol'])
+assert.deepStrictEqual(words('wol.lol lo,lol'), [ 'wol.lol', 'lo,lol' ])
 // End of tests */

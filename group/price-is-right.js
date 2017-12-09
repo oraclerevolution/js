21edfa3a6dd9367584643ad9f82ca7e2d0c3a106
oraclerevolution <mac.n.drix@gmail.com>
2017-12-09 16:43:13 +0000

    le juste prix

--- group/price-is-right.js
+++ group/price-is-right.js
@@ -12,7 +12,38 @@
  * Create a function `getCostMl` that takes a quantity in milliliters
  * and return the cost (and applying the 3 for 1000 FCFA discount)
  */
+const getCostCount = numberOfCans => {
+  let calcul
 
+  if (numberOfCans === 3) {
+    calcul = '1000 francs'
+  } else {
+    calcul = numberOfCans * 650
+  }
 
+  return calcul + ' francs'
+}
+
+console.log(getCostCount(5))
+console.log(getCostCount(3))
+
+const getCostMl = quantity => {
+  let result
+
+  if (quantity === 1500) {
+    result = '1000'
+  } else if (quantity != 1500) {
+    const division = quantity / 500
+
+    result = division * 650
+  }
+
+  return result + ' francs'
+}
+
+console.log(getCostMl(1500))
+console.log(getCostMl(500))
+console.log(getCostMl(2000))
 // You must write your own tests
+
 throw Error('No tests !')

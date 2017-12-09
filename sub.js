6af523e45207a7dfdced6e86038bdb023c149118
oraclerevolution <mac.n.drix@gmail.com>
2017-12-09 18:18:54 +0000

    apport

--- sub.js
+++ sub.js
@@ -14,7 +14,17 @@ console.log(sub(2, 2))
 //* Begin of tests
 const assert = require('assert')
 const rand = Math.random()
-
+for (var i=1; i <= 20; i++)
+//{
+//    if (i % 15 == 0)
+//        console.log("FizzBuzz");
+ //   else if (i % 3 == 0)
+//        console.log("Fizz");
+  //  else if (i % 5 == 0)
+    //    console.log("Buzz");
+    //else
+      //  console.log(i);
+//}
 assert.strictEqual(typeof sub, 'function')
 assert.strictEqual(sub.length, 2)
 assert.strictEqual(sub(2, 2), 0)

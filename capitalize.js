bfe705526287e98a41ab5b6077155979e2c04ff9
oraclerevolution <34175335+oraclerevolution@users.noreply.github.com>
2017-12-10 09:37:43 +0000

    Update capitalize.js

--- capitalize.js
+++ capitalize.js
@@ -6,10 +6,6 @@
  *
  * @next jaden-case
  */
-//function Premiere(chaine){
-//    return chaine.substr(0,1).toUpperCase()+	chaine.substr(1,chaine.length).toLowerCase()		}
-//
-//alert(Premiere('dfEZRZSDGFSD4E'))
 const capitalize = firstCharacter => firstCharacter.charAt(0).toUpperCase() + firstCharacter.substr(1, firstCharacter.length)
 
 console.log(capitalize('oracle'))
